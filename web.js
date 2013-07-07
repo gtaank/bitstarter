4ffbbff83c31598f6a48a55306ef4885cbacb3be
gtaank <gtaank@gmail.com>
2013-07-07 21:47:32 +0530

    updated web.js

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var data;
+    fs.readFile('index.html', function(err,data){if(err) throw err;})
+  response.send('Hello World 2!'+ data);
 });
 
 var port = process.env.PORT || 5000;
