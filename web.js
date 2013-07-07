fee43db68d604bfe4998350a45b415b5468aca6f
gtaank <gtaank@gmail.com>
2013-07-07 22:00:06 +0530

    updated web.js

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
     
-  response.send(fs.readFile('index.html', function(err,data){if(err) throw err;return data;}));
+  response.send("Trying to read index" + (fs.readFile('index.html', function(err,data){if(err) throw err;return data;})));
 });
 
 var port = process.env.PORT || 5000;
